057dc71ea05f4c13c690f54382bf7fd71843082c
Eshan Jayasundara <115539854+EshanJayasundara@users.noreply.github.com>
2023-08-19 16:28:29 +0530

    Update TestController.java

--- src/main/java/com/MedicalClinic/LifeCare/controllers/TestController.java
+++ src/main/java/com/MedicalClinic/LifeCare/controllers/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class TestController {
   @PostMapping("/all")
   public String allAccess(@RequestBody String hello) {
