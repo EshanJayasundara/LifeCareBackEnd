ff501d13d25bc890da88bb1df415581857d827b6
Eshan Jayasundara <115539854+EshanJayasundara@users.noreply.github.com>
2023-08-19 15:55:21 +0530

    Update LifCareWebApp.java

--- src/main/java/com/MedicalClinic/LifeCare/LifCareWebApp.java
+++ src/main/java/com/MedicalClinic/LifeCare/LifCareWebApp.java
@@ -3,6 +3,7 @@ package com.MedicalClinic.LifeCare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 public class LifCareWebApp {
 
